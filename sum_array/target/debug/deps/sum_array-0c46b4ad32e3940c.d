3ba46eb6ef70ee48218df79370993b25d28bf579
Gu-nunes12 <gustavo.nunes.94515@a.fecaf.com.br>
2025-03-23 23:39:35 +0000

    Código corrigido

---
+++ sum_array/target/debug/deps/sum_array-0c46b4ad32e3940c.d
@@ -0,0 +1,5 @@
+/workspaces/Gustavo.nunes.ads3na/sum_array/target/debug/deps/sum_array-0c46b4ad32e3940c: src/lib.rs
+
+/workspaces/Gustavo.nunes.ads3na/sum_array/target/debug/deps/sum_array-0c46b4ad32e3940c.d: src/lib.rs
+
+src/lib.rs:
---
+++ sum_array/target/debug/deps/sum_array-ec8fceaa4b54b5b2.d
@@ -0,0 +1,7 @@
+/workspaces/Gustavo.nunes.ads3na/sum_array/target/debug/deps/libsum_array-ec8fceaa4b54b5b2.rmeta: src/lib.rs
+
+/workspaces/Gustavo.nunes.ads3na/sum_array/target/debug/deps/libsum_array-ec8fceaa4b54b5b2.rlib: src/lib.rs
+
+/workspaces/Gustavo.nunes.ads3na/sum_array/target/debug/deps/sum_array-ec8fceaa4b54b5b2.d: src/lib.rs
+
+src/lib.rs:
