d862e64b3e8c06bc58c45542a1d1d884edb608b7
Gu-nunes12 <gustavo.nunes.94515@a.fecaf.com.br>
2025-04-04 18:31:22 +0000

    codigo feito

---
+++ desafio_arvore/target/debug/deps/desafio_arvore-4932ac3854c82e3e.d
@@ -0,0 +1,7 @@
+/workspaces/Gustavo.nunes.ads3na/desafio_arvore/target/debug/deps/libdesafio_arvore-4932ac3854c82e3e.rmeta: src/lib.rs
+
+/workspaces/Gustavo.nunes.ads3na/desafio_arvore/target/debug/deps/libdesafio_arvore-4932ac3854c82e3e.rlib: src/lib.rs
+
+/workspaces/Gustavo.nunes.ads3na/desafio_arvore/target/debug/deps/desafio_arvore-4932ac3854c82e3e.d: src/lib.rs
+
+src/lib.rs:
---
+++ desafio_arvore/target/debug/libdesafio_arvore.d
@@ -0,0 +1,1 @@
+/workspaces/Gustavo.nunes.ads3na/desafio_arvore/target/debug/libdesafio_arvore.rlib: /workspaces/Gustavo.nunes.ads3na/desafio_arvore/src/lib.rs
